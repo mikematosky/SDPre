f8ca5daa555ad5dcd9fa8575dc728c8b749548ea
mikematosky <mikematosky@hotmail.com>
2020-02-25 05:23:28 -0500

    finish jthw ex 6

---
+++ JavaTheHardWay/MathOperations.java
@@ -0,0 +1,50 @@
+public class MathOperations {
+  public static void main( String[] args ) {
+    int a,b,c,d,e,f,g;
+    double x, y, z;
+    String one, two, both;
+
+    a=10;
+    b=27;
+    System.out.println("ais"+a+",bis"+b);
+
+    c=a+b;
+    System.out.println( "a+b is " + c );
+    d=a-b;
+    System.out.println( "a-b is " + d );
+    e = a+b*3;
+    System.out.println( "a+b*3 is " + e );
+    f=b/2;
+    System.out.println( "b/2 is " + f );
+    g=b%10;
+    System.out.println( "b%10 is " + g );
+
+    x=1.1;
+    System.out.println( "\nx is "+x);
+    y=x*x;
+    System.out.println( "x*x is "+y);
+    z=b/2;
+    System.out.println( "b/2 is "+z);
+    System.out.println();
+
+    one = "dog";
+    two = "house";
+    both = one + two;
+    System.out.println( both );
+    System.out.println();
+
+    //new stuff for the exercise
+
+    int v,w;
+
+    v=5;
+    w=12;
+
+    System.out.println("v+w= "+(v+w));
+    System.out.println("v-w= "+(v-w));
+    System.out.println("v/w= "+(v/w));
+    System.out.println("v*w= "+(v*w));
+    System.out.println("w%v= "+(w%v));
+    
+  }
+}
