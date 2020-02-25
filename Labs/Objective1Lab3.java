4a320375e5504ce86f3ddc0e9db361e71416a7d2
mikematosky <mikematosky@hotmail.com>
2020-02-25 04:56:40 -0500

    complete Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+class Objective1Lab3{
+  public static void main(String[] args){
+    System.out.println("Name: Mike Matosky");
+    System.out.println("Age: 33");
+    System.out.println("State: Ohio");
+  }
+}
