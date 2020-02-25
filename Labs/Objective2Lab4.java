6afbe5f1bae31f35b7089db6540a334fe1e3ee1d
mikematosky <mikematosky@hotmail.com>
2020-02-25 05:59:13 -0500

    completed Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,12 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1= 44;
+    num2= 6.6;
+    num3= 64.387;
+    average= (num1+num2+num3)/3;
+
+    System.out.println("The average of "+num1+", "+num2+", and "+num3+" is   "+average);
+  }
+}
