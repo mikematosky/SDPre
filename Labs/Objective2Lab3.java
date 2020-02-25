8c9f936899ea4cd7da062f61727cb7a09a3a4661
mikematosky <mikematosky@hotmail.com>
2020-02-25 05:46:08 -0500

    completed Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,18 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+
+    food1= "Mac n Cheese";
+    food2= "Pizza";
+    food3= "Rice Krispy Treats";
+
+    System.out.println("My favorite foods are:");
+    System.out.print(food1+"\n"+food2+"\n"+food3+"\n"); //playing with escapes
+
+    /*
+      using \n in a print command needed to end in \n for the terminal or else
+      it would have an offset %. Interesting... It doesn't affect the output-
+      just the terminal window because the line never ended. Probably a mac thing
+    */
+  }
+}
