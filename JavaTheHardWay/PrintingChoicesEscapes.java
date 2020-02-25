e0e674c4447aeab3d9871e52ba4ed5b36f13b815
mikematosky <mikematosky@hotmail.com>
2020-02-25 04:07:50 -0500

    finish jthw ex 4.2

---
+++ JavaTheHardWay/PrintingChoicesEscapes.java
@@ -0,0 +1,36 @@
+
+
+
+/*
+public class PrintingChoices{
+  public static void main(String[] args){
+    System.out.println("Alpha");
+    System.out.println("Bravo");
+
+    System.out.println("Charlie");
+    System.out.println("Delta");
+    System.out.println("");
+
+    System.out.print("Echo");
+    System.out.print("Foxtrot");
+
+    System.out.println("Golf");
+    System.out.print("Hotel");
+    System.out.println("");
+    System.out.println("India");
+
+    System.out.println("");
+    System.out.println("This"+" "+"is"+" "+"a"+" "+"test.");
+    /*
+      Choosing to omit the spaces between ""s for compact readability
+
+    System.out.println("I am"+" learning Java"+" "+"the Hard "+"Way!");
+  }
+}
+*/
+
+public class PrintingChoicesEscapes{
+  public static void main(String[] args){
+    System.out.println("Alpha\nBravo\nCharlie\nDelta\n\nEchoFoxtrotGolf\nHotel\nIndia\n\nThis is a test.\nI am learning Java the Hard Way!");
+  }
+}
