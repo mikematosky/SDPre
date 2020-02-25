c05350eabb429fabfab4701ea000ba3dd5c68699
mikematosky <mikematosky@hotmail.com>
2020-02-25 05:35:47 -0500

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Mike Matosky";
+    int age = 33;
+    String state = "Ohio";
+
+    System.out.println("Name: "+name);
+    System.out.println("Age: "+age);
+    System.out.println("State: "+state);
+  }
+}
