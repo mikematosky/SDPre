ffd718b82c5ce97462cebf3c6bf6207025728d06
mikematosky <mikematosky@hotmail.com>
2020-02-25 04:19:55 -0500

    finish jthw ex 1

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg{
   public static void main(String[] args){
     System.out.println("I am determined to learn how to code.");
-    System.out.println("Today's date is February 24th");
+    System.out.println("Today's date is Monday February 24th");
   }
 }
