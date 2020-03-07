0a172a88e801dc65c7577f198cf153142a52e295
mikematosky <mikematosky@hotmail.com>
2020-03-06 19:06:26 -0500

    completed Objective6Lab3

---
+++ Labs/Objective6Lab3.java
@@ -0,0 +1,20 @@
+public class Objective6Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+    while(counter <= 20){
+      System.out.print(counter);
+
+      if(counter % 2 == 0)
+        System.out.print(" is even\n");
+      else
+        System.out.print(" is odd\n");
+
+      counter++;
+
+    }
+    /*
+      Your solution goes here
+    */
+  }
+}
