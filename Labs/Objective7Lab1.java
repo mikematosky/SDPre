5e20e9f160155ccbeba3995bfe6f846e5ea3868f
mikematosky <mikematosky@hotmail.com>
2020-03-06 19:54:26 -0500

    completed Objective7Lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,9 @@
+public class Objective7Lab1 {
+  public static void main(String[] args){ //starter code was also missing this bracket
+    printName();
+  }
+
+  public static void printName() {
+    System.out.println("Hello Doug");
+  }
+}
